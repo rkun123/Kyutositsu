5e34a9d580b73ef524293441c8c9567877dd8030
rkun123 <tren111213@gmail.com>
2021-01-14 23:58:46 +0900

    [add] react-redux

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,24 +2,19 @@ import React, { useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import store from './store/index'
+import { RootState } from './store/index'
 import Callback from './pages/Callback'
+import { useSelector } from 'react-redux';
 
 function App() {
-  const [ avatarURL, setAvatarURL ] = useState<string>('')
-
-  useEffect(() => {
-    store.subscribe(() => {
-      setAvatarURL(store.getState().user.image)
-    })
-  })
+  const user = useSelector((state: RootState) => state.user)
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>C3SNS</h1>
         <a href="http://localhost:3000/api/v1/auth/discord?auth_origin_url=http://localhost:3001/callback">Discord Login</a>
-        <img src={avatarURL}></img>
+        <img src={user.image}></img>
       </header>
       <Router>
         <Switch>
--- frontend/src/pages/Callback.tsx
+++ frontend/src/pages/Callback.tsx
@@ -1,36 +1,39 @@
 import { useEffect } from "react"
-import store from '../store/index'
+import { useDispatch, useSelector } from "react-redux"
+import { RootState } from "../store"
 import { actions as authActions, Auth} from '../store/auth'
 import { actions as userActions, User } from '../store/user'
 import apiClientGenerator from '../utils/apiClientGenerator'
 
 function Callback() {
-    async function fetchUserAndStore() {
-        const http = apiClientGenerator(store.getState().auth)
+    const auth = useSelector((state: RootState) => state.auth)
+    const user = useSelector((state: RootState) => state.user)
+    const dispatch = useDispatch()
+
+    async function fetchUserAndStore(auth: Auth) {
+        const http = apiClientGenerator(auth)
         const res = await http.get('/auth/validate_token')
         console.log(res.data)
         if(res.data.success === true) {
             const user = res.data.data as User
-            console.warn(user)
-            store.dispatch(userActions.setUser(user))
+            console.info('Success', user)
+            dispatch(authActions.setAuth(auth))
+            dispatch(userActions.setUser(user))
         }
-
-        console.info(store.getState().user)
     }
 
     useEffect(() => {
         async function f() {
             const param = new URLSearchParams(window.location.search)
-            store.dispatch(authActions.setAuth({
+            const auth = {
                 authToken: param.get('auth_token')!,
                 client: param.get('client_id')!,
                 uid: param.get('uid')!
-            }))
-            console.info(store.getState())
-            await fetchUserAndStore()
+            }
+            await fetchUserAndStore(auth)
         }
         f()
-    })
+    }, [])
 
     return (
         <div>
