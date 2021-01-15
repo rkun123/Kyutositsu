ecd957626fbf14eb5fe00ba36ea2f7d0ddd0df8c
rkun123 <tren111213@gmail.com>
2021-01-15 18:09:30 +0900

    [update] Post - User association

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
+  has_many :posts
+
   include DeviseTokenAuth::Concerns::User
   devise :rememberable, :omniauthable
 
