09fef417ad285d205b7fda9a0794682c5f4a15e8
rkun123 <tren111213@gmail.com>
2021-01-15 21:40:21 +0900

    [add] Material-UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,55 @@
 import React, { useState, useEffect} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { RootState } from './store/index'
 import Callback from './pages/Callback'
+import Home from './pages/Home'
 import { useSelector } from 'react-redux';
+import { AppBar, makeStyles, Toolbar, Typography, Button, Avatar, Container } from '@material-ui/core';
+
+const useStyle = makeStyles((theme) => ({
+  appBar: {
+    position: 'relative',
+    flexGrow: 1
+  },
+  title: {
+    flexGrow: 1
+  }
+}))
 
 function App() {
-  const user = useSelector((state: RootState) => state.user.user)
+  const userState = useSelector((state: RootState) => state.user)
+  const classes = useStyle()
+
+  const loginButton = () => {
+    if(!userState.success) {
+      return (
+        <Button color="inherit" href="http://localhost:3000/api/v1/auth/discord?auth_origin_url=http://localhost:3001/callback">
+          Login
+        </Button>
+      )
+    }
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>C3SNS</h1>
-        <a href="http://localhost:3000/api/v1/auth/discord?auth_origin_url=http://localhost:3001/callback">Discord Login</a>
-        <img src={user.image}></img>
-      </header>
-      <Router>
-        <Switch>
-          <Route path="/callback"><Callback /></Route>
-        </Switch>
-      </Router>
+      <AppBar position="absolute" color="default" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" color="inherit" className={classes.title}>
+            C3SNS
+          </Typography>
+          {loginButton()}
+          <Avatar src={userState.user.image}></Avatar>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth="sm">
+        <Router>
+          <Switch>
+            <Route path="/callback"><Callback /></Route>
+            <Route path="/"><Home /></Route>
+          </Switch>
+        </Router>
+      </Container>
     </div>
   );
 }
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+function Home() {
+    return (
+        <React.Fragment>
+            <h1>Home</h1>
+        </React.Fragment>
+    )
+}
+
+export default Home
