31219905283ad0cd54e5f6f6b3b845d65800493c
rkun123 <tren111213@gmail.com>
2021-01-21 20:07:29 +0900

    [add] タグのない投稿を許可しないようValidationを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
     belongs_to :user
     has_many :taggings, dependent: :destroy
     has_many :tags, through: :taggings
+
+    validate :post_without_tags_is_disallowed
+
+    private
+
+        def post_without_tags_is_disallowed
+            errors.add(:taggings, 'Post without tags is not allowed') if taggings.count == 0
+        end
+
 end
