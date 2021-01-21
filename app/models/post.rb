45c9f8d98095f25d8f0eaedead71ecb4efd67ad1
rkun123 <tren111213@gmail.com>
2021-01-21 23:29:44 +0900

    [fix] Tagのvalidationが常に失敗していたので修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,11 +4,12 @@ class Post < ApplicationRecord
     has_many :tags, through: :taggings
 
     validate :post_without_tags_is_disallowed
+    validates :content, length: { minimum: 1 }
 
     private
 
         def post_without_tags_is_disallowed
-            errors.add(:taggings, 'Post without tags is not allowed') if taggings.count == 0
+            errors.add(:tags, 'Post without tags is not allowed') if tags.size == 0
         end
 
 end
