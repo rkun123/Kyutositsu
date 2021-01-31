49bec14452107e90f538177bac02f1b49803e813
rkun123 <tren111213@gmail.com>
2021-01-31 21:31:23 +0900

    [fix] build fail

--- frontend/config/paths.js
+++ frontend/config/paths.js
@@ -63,7 +63,7 @@ module.exports = {
   yarnLockFile: resolveApp('yarn.lock'),
   testsSetup: resolveModule(resolveApp, 'src/setupTests'),
   proxySetup: resolveApp('src/setupProxy.js'),
-  appNodeModules: resolveApp('node_modules'),
+  appNodeModules: resolveApp('../node_modules'),
   swSrc: resolveModule(resolveApp, 'src/service-worker'),
   publicUrlOrPath,
 };
